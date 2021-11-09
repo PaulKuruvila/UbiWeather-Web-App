be4c10573b84818e58969e86026a119418838f20
Paul Kuruvila <paul.paulkuruvila@gmail.com>
2021-11-09 00:27:38 -0600

    UI Update
    Updated textbox aesthetic and UI mobile responsiveness.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -10,15 +10,13 @@ function Navbar() {
                     <div className="TitleLogo"></div>
                     <h1 className="TitleText">UbiWeather</h1>
                 </li>
-                <li className="Options">
-                    <li className="Search">
-                        <input className="SearchBar" type="text" placeholder="Please enter a city name." />
-                        <button className="SearchButton" type="submit">Search</button>
-                    </li>
-                    <li className="Icons">
-                        <a href="https://github.com/PaulKuruvila" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
-                        <a href="https://linkedin.com/in/paulkuruvila" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin"></i></a>
-                    </li>
+                <li className="Search">
+                    <input className="SearchBar" type="text" placeholder="Please enter a city name." />
+                    <button className="SearchButton" type="submit">Search</button>
+                </li>
+                <li className="Icons">
+                    <a href="https://github.com/PaulKuruvila" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
+                    <a href="https://linkedin.com/in/paulkuruvila" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin"></i></a>
                 </li>
             </ul>
         </nav>
