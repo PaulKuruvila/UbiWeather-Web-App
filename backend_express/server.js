3d4d4cf6aa71cc2d65a0288ffb9f9f6a2d9ba25b
Paul Kuruvila <paul.paulkuruvila@gmail.com>
2022-08-15 18:31:40 -0500

    Fix environment variable config

--- backend_express/server.js
+++ backend_express/server.js
@@ -2,16 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const app = express();
-require('dotenv').config({ path: path.resolve(__dirname, '../env') })
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
 const port = process.env.PORT || 5000;
-//const API_KEY = process.env.API_KEY;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend_react/build')));
 
 app.get("/api", (req, res) => {
-    res.json({message: `Fake api key is ${process.FAKE_API_KEY}!!!`})
+    res.json({message: `Fake api key is ${process.env.FAKE_API_KEY}!!!`})
 });
 
 // For any GET requests that are not handled, return to home page (Needs to be last)
