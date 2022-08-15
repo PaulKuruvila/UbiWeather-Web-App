076a6036c892a7ced852232b2a9f2ec51418fb02
Paul Kuruvila <paul.paulkuruvila@gmail.com>
2022-08-15 18:37:27 -0500

    Test heroku environment variable config

--- backend_express/server.js
+++ backend_express/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const app = express();
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
