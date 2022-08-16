54d33e89a45c1ea50bb3f78405ee6e33539f2de9
Paul Kuruvila <paul.paulkuruvila@gmail.com>
2022-08-15 20:04:47 -0500

    Update directory to client build

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../frontend_react/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get("/api", (req, res) => {
     res.json({message: `Fake api key is ${process.env.FAKE_API_KEY}!!!`})
@@ -15,7 +15,7 @@ app.get("/api", (req, res) => {
 
 // For any GET requests that are not handled, return to home page (Needs to be last)
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend_react/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(port, () => {console.log(`Server started on port ${port}`)});
